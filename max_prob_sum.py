bf4c48e397daa5eea08116c05cd34fee9b402124
csuraparaju <csuraparaju@wpi.edu>
2023-04-24 19:34:59 -0400

    max prob sum backtrace

--- max_prob_sum.py
+++ max_prob_sum.py
@@ -19,9 +19,72 @@ Returns:
 
 
 # Helper function to compute the score of a base pair (Ri, Rj)
-def computePairScores(Rj, Ri, i, j, probL, probR, probP):
-  # set of possible rna base pairs
-  possibleBasePairs = set(['AU', 'UA', 'GC', 'CG', 'GU', 'UG'])
+# def computePairScores(Rj, Ri, i, j, probL, probR, probP):
+#   # set of possible rna base pairs
+#   possibleBasePairs = set(['AU', 'UA', 'GC', 'CG', 'GU', 'UG'])
+
+#   if Ri + Rj in possibleBasePairs:
+#     return probL[i] + probR[j]
+#   else:
+#     return probP[i] + probP[j]
+
+
+# def MaximumProbabilitySum(probL, probR, probP, seq):
+#   n = len(seq)
+#   N = [[0 for i in range(n)] for j in range(n)]
+#   for i in range(n):
+#     for j in range(n):
+#       if i == j:
+#         N[i][j] = probP[i]
+#       elif j < i:
+#         N[i][j] = 0
+#       else:
+#         N[i][j] = max(
+#           N[i + 1][j] + probP[i],
+#           N[i][j - 1] + probP[j], 
+#           N[i + 1][j - 1] + computePairScores(seq[i], seq[j], i, j, probL, probR, probP),
+#           max([N[i][k] + N[k + 1][j] for k in range(i, j)]))
+#   return N
+
+# # Backtracking function to find the optimal dot bracket representation of the RNA sequence
+# # from the DP table
+# def BackTrack(i, j, fold, N, seq):
+#   if j <= i:
+#     return
+#   elif N[i][j] == N[i + 1][j] + probP[i]:
+#     BackTrack(i + 1, j, fold, N, seq)
+#   elif N[i][j] == N[i][j - 1] + probP[j]:
+#     BackTrack(i, j - 1, fold, N, seq)
+#   elif N[i][j] == N[i + 1][j - 1] + computePairScores(seq[i], seq[j], i, j, probL, probR, probP):
+#     fold[i] = '('
+#     fold[j] = ')'
+#     BackTrack(i + 1, j - 1, fold, N, seq)
+#   else:
+#     for k in range(i, j):
+#       if N[i][j] == N[i][k] + N[k + 1][j]:
+#         BackTrack(i, k, fold, N, seq)
+#         BackTrack(k + 1, j, fold, N, seq)
+#         break
+  
+
+# # Main function to run the Enhanced Nussinov algorithm
+# def EnhancedNussinov(probL, probR, probP, seq):
+#   n = len(seq)
+#   N = MaximumProbabilitySum(probL, probR, probP, seq)
+#   fold = ['.' for i in range(n)]
+#   BackTrack(0, n-1, fold, N, seq)
+#   return fold
+
+import numpy as np
+import sys
+
+
+
+# Checks if pair is complementary and assigns the appropriate score as defined in the paper
+def isPairScore(seq, i, j, probL, probR, probP):
+  possibleBasePairs = set(['AU', 'UA', 'GC', 'CG', 'GU', 'UG']) # includes wobble pairs
+  Ri = seq[i]
+  Rj = seq[j]
 
   if Ri + Rj in possibleBasePairs:
     return probL[i] + probR[j]
@@ -29,52 +92,82 @@ def computePairScores(Rj, Ri, i, j, probL, probR, probP):
     return probP[i] + probP[j]
 
 
-def MaximumProbabilitySum(probL, probR, probP, seq):
-  n = len(seq)
-  N = [[0 for i in range(n)] for j in range(n)]
-  for i in range(n):
-    for j in range(n):
-      if i == j:
-        N[i][j] = probP[i]
-      elif j < i:
-        N[i][j] = 0
+# Initializes an n x n matrix
+def initMatrix(Text : str) -> list[list[int]]:
+  n = len(Text)
+
+  M = np.empty([n, n])
+  M[:] = np.NAN
+
+  M[range(n), range(n)] = 0
+  M[range(1, n), range(n - 1)] = 0
+
+  return M
+
+# Forward direction for Updated Nussinov algorithm
+def Forward(probL, probR, probP, M, Text, min_loop_length = 0):
+  n = len(Text)
+
+  for k in range(1, n):
+    for i in range(n - k):
+      j = i + k
+
+      if (j - i >= min_loop_length):
+        down = M[i + 1][j]
+        left = M[i][j - 1]
+        diag = M[i + 1][j - 1] + isPairScore(Text, i, j, probL, probR, probP)
+        other = max([M[i][t] + M[t + 1][j] for t in range(i, j)])
+
+        M[i][j] = max(down, left, diag, other)
       else:
-        N[i][j] = max(
-          N[i + 1][j] + probP[i],
-          N[i][j - 1] + probP[j], 
-          N[i + 1][j - 1] + computePairScores(seq[i], seq[j], i, j, probL, probR, probP),
-          max([N[i][k] + N[k + 1][j] for k in range(i, j)]))
-  return N
+        M[i][j] = 0
 
-# Backtracking function to find the optimal dot bracket representation of the RNA sequence
-# from the DP table
-def BackTrack(i, j, fold, N, seq):
-  if i >= j:
-    return
-  elif N[i][j] == N[i + 1][j] + probP[i]:
-    BackTrack(i + 1, j, fold, N, seq)
-  elif N[i][j] == N[i][j - 1] + probP[j]:
-    BackTrack(i, j - 1, fold, N, seq)
-  elif N[i][j] == (N[i + 1][j - 1] + computePairScores(seq[i], seq[j], i, j, probL, probR, probP)):
-    fold[i] = '('
-    fold[j] = ')'
-    BackTrack(i + 1, j - 1, fold, N, seq)
-  else:
-    for k in range(i + 1, j - 1):
-      if N[i][j] == N[i][k] + N[k + 1][j]:
-        BackTrack(i, k, fold, N, seq)
-        BackTrack(k + 1, j, fold, N, seq)
-        break
+  return M
 
-  return fold
+# Back direction of Updated Nussinov algorithm, backtrack
+def Back(M, Text, pairs, i, length, probL, probR, probP):
+  j = length
 
-# Main function to run the Enhanced Nussinov algorithm
-def EnhancedNussinov(probL, probR, probP, seq):
-  n = len(seq)
-  N = MaximumProbabilitySum(probL, probR, probP, seq)
-  fold = ['.' for i in range(n)]
-  fold = BackTrack(0, n - 1, fold, N, seq)
-  return ''.join(fold)
+  if i < j:
+    if M[i][j] == M[i + 1][j]:
+      Back(M, Text, pairs, i + 1, j, probL, probR, probP)
+    elif M[i][j] == M[i][j - 1]:
+      Back(M, Text, pairs, i, j - 1, probL, probR, probP)
+    elif M[i][j] == M[i + 1][j - 1] + isPairScore(Text, i, j, probL, probR, probP):
+      pairs.append((i, j))
+      Back(M, Text, pairs, i + 1, j - 1, probL, probR, probP)
+    else:
+      for k in range(i + 1, j - 1):
+        if M[i][j] == M[i, k] + M[k + 1][j]:
+          Back(M, Text, pairs, i, k, probL, probR, probP)
+          Back(M, Text, pairs, k + 1, j, probL, probR, probP)
+          break
 
 
-    
+# Convert to dotBracket
+def dotBracket(Text : str, pairs : list[tuple[int, int]]) -> str:
+  dot = ["." for i in range(len(Text))]
+
+  for s in pairs:
+    dot[max(s)] = ")"
+    dot[min(s)] = "("
+
+  return "".join(dot)
+
+# Run Updated Nussinov algorithm
+def UpdatedNussinov(Text, probL, probR, probP):
+  n = len(Text)
+
+  initial_matrix = initMatrix(Text)
+  forward_matrix = Forward(probL, probR, probP, initial_matrix, Text)
+
+  pairs = []
+  Back(forward_matrix, Text, pairs, 0, n - 1, probL, probR, probP)
+
+  return dotBracket(Text, pairs)
+
+# probL = [0.5, 0.5, 0.5, 0.5, 0.5]
+# probR = [0.5, 0.5, 0.5, 0.5, 0.5]
+# probP = [0.5, 0.5, 0.5, 0.5, 0.5]
+# seq = "GGGGG"
+# print(UpdatedNussinov(seq, probL, probR, probP))
